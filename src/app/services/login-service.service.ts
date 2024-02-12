e8c36df82257ee8806474be82656ea2a17214cc9
Mazharul Islam Rakib <mirakib33@gmail.com>
2024-02-12 11:48:13 +0600

    login service updated

--- src/app/services/login-service.service.ts
+++ src/app/services/login-service.service.ts
@@ -5,5 +5,39 @@ import { Injectable } from '@angular/core';
 })
 export class LoginServiceService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8083/login-service/api';
+
+  constructor(private http: HttpClient) {}
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error);
+    return Promise.reject(error.message || error);
+  }
+
+  // GET request
+  get(endpoint: string): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${endpoint}`).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  // POST request
+  login(loginData: Login): Observable<any> {
+    return this.http.post(`${this.baseUrl}/login`, loginData);
+  }
+
+  // PUT request
+  put(endpoint: string, data: any): Observable<any> {
+    return this.http.put(`${this.baseUrl}/${endpoint}`, data).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  // DELETE request
+  delete(endpoint: string): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${endpoint}`).pipe(
+      catchError(this.handleError)
+    );
+  }
+
 }
